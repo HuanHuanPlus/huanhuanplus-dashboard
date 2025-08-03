25fe0d2bec2580370eca92b7b1a73fb5a51fca28
HuanHuanPlus <huanhuanplus@gmail.com>
2025-08-04 02:08:34 +0700

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div style="padding: 2rem; text-align: center;">
+    <h1>Selamat Datang di HuanHuan Plus</h1>
+    <p>Dashboard Penjualan Otomatis Siap Digunakan!</p>
+  </div>
+</template>
+
+Tambah halaman utama index.vue
